4c3e6e5cf3f0c6b29338d4b3fc510976608b1068
jakabadambalazs <jakabadambalazs@gmail.com>
2016-08-09 12:15:09 +0200

    remote sync

--- custom/metadata/mkt_worklogs_meetingsMetaData.php
+++ custom/metadata/mkt_worklogs_meetingsMetaData.php
@@ -1,5 +1,5 @@
 <?php
-// created: 2016-03-02 08:12:49
+// created: 2016-08-09 09:56:37
 $dictionary["mkt_worklogs_meetings"] = array (
   'true_relationship_type' => 'one-to-many',
   'relationships' => 
