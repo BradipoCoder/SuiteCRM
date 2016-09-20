07ad62e22c5554dca08faef13e94b97e50b5b147
jakabadambalazs <jakabadambalazs@gmail.com>
2016-09-20 15:17:19 +0200

    remote sync

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_chiamatoperbarbara_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2016-09-02 10:06:46
+$dictionary['Account']['fields']['chiamatoperbarbara_c']['inline_edit']='1';
+$dictionary['Account']['fields']['chiamatoperbarbara_c']['labelValue']='Contatto TM';
+
+ ?>
